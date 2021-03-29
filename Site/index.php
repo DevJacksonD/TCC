0745bb2c573420f760a732ae572ed9588819b050
Jackson Dias <dias.jackson@pucpr.edu.br>
2021-03-29 11:16:20 -0300

    Adicionado index.php

---
+++ Site/index.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+		<meta http-equiv="X-UA-Compatible" content="ie-edge" />
+		<link rel="stylesheet" href="./style.css" />
+		<title>Monitoramento de Vibrações</title>
+	</head>
+	<body>
+		<header class="header">
+			<a href="/">Logo</a>
+			<nav>
+				<ul class="menu">
+					<li><a href="tabeladados.php">Tabela de dados</a></li>
+					<li><a href="grafico.php">Gráficos</a></li>
+					<li><a href="/">Análise</a></li>
+				</ul>
+			</nav>
+		</header>
+	</body>
+<html>
+			
